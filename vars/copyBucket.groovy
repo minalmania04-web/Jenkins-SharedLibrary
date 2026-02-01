b64fabfffab29de0a8188b8e364266737b74aff5
minalmania04-web <mina.lmania04@gmail.com>
2026-02-01 21:01:50 +0100

    Update copyBucket.groovy

--- vars/copyBucket.groovy
+++ vars/copyBucket.groovy
@@ -1,8 +1,10 @@
 def Transfer_bucket_Content() {
     sh '''
+       echo "hello this is buckets scaning"
         BUCKETS_NAME=$(aws s3api list-buckets --query "Buckets[].Name" --output text)
 
         for BUCKET_NAME in $BUCKETS_NAME; do
+        echo "this is bucket copy"
             aws s3 cp \
               "s3://$BUCKET_NAME/" \
               "s3://$BUCKET_NAME_NEW/" \
