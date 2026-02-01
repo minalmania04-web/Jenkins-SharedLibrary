050e25164085f0f7e27cf44b382fea719e9d08ef
minalmania04-web <mina.lmania04@gmail.com>
2026-02-01 21:57:49 +0100

    Update copyBucket.groovy

--- vars/copyBucket.groovy
+++ vars/copyBucket.groovy
@@ -1,15 +1,17 @@
 def call() {
-    sh '''
-       echo "hello this is buckets scaning"
+     sh '''
+        set -euo pipefail
+
+        echo "Scanning all buckets..."
         BUCKETS_NAME=$(aws s3api list-buckets --query "Buckets[].Name" --output text)
 
         for BUCKET_NAME in $BUCKETS_NAME; do
-        echo "this is bucket copy"
-            aws s3 cp \
-              "s3://$BUCKET_NAME/" \
-              "s3://${BUCKET_NAME}-new" \
-              --recursive \
-              --quiet
+            # ne traiter que les buckets qui se terminent par '-kitty'
+            if [[ "$BUCKET_NAME" == *"-kitty" ]]; then
+                DEST_BUCKET="${BUCKET_NAME}-new"
+                echo "Copying content from $BUCKET_NAME to $DEST_BUCKET"
+                aws s3 cp "s3://$BUCKET_NAME/" "s3://$DEST_BUCKET/" --recursive --quiet
+            fi
         done
     '''
 }
