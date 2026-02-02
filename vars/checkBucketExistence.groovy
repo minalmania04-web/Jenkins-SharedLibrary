a936ee2f939adf032f1b48ed1fe8546ad381a393
minalmania04-web <mina.lmania04@gmail.com>
2026-02-02 18:02:02 +0100

    Update checkBucketExistence.groovy

--- vars/checkBucketExistence.groovy
+++ vars/checkBucketExistence.groovy
@@ -1,15 +1,15 @@
-def call(String bucketName)
-{
-echo "verify the existance of the : ${bucketName}"
-  def status = sh(
-   script: "aws s3api head-bucket --bucket s://${bucketName} --region "eu-west-1"
-    returnStatus: true
-  )
-if(status == 0)
-  {
-    echo "ok , bucket existe"
-  } else {
-    echo "bucket doesn't exist"
-  }
-  
+def call(String bucketName) {
+    echo "Verify the existence of the bucket: ${bucketName}"
+
+    def status = sh(
+        script: "aws s3api head-bucket --bucket ${bucketName} --region eu-west-1",
+        returnStatus: true
+    )
+
+    if (status == 0) {
+        echo "OK, bucket exists"
+    } else {
+        echo "Bucket doesn't exist"
+    }
 }
+
