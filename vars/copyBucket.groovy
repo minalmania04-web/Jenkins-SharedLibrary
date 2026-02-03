00e4057ee218b9f37e59c998730bbe1571d093b1
minalmania04-web <mina.lmania04@gmail.com>
2026-02-03 21:28:51 +0100

    Update copyBucket.groovy

--- vars/copyBucket.groovy
+++ vars/copyBucket.groovy
@@ -1,30 +1,87 @@
-def call(Map config = [:]) 
-{
-   if(config.envName != "dev")
-     {
-          error "this is not dev env"
-     }
-    if(!config.sourceBucket)
-     {
-          error "source bucket empty"
-     }
-     if(!config.targetBucket)
-     {
-          error "target bucket empty"
-     }
-   echo " input no empty"
-def sourceBucket = config.sourceBucket
-def targetBucket = config.targetBucket
 
-     checkBucketExistence(sourceBucket)
-     checkBucketExistence(targetBucket)
-     
-     echo "this step will copy the content of ${sourceBucket} to the target ${targetBucket}"
+def call(String sourceBucket, String targetBucket, enviroment = "dev") {
+    def config = [
+        source: sourceBucket,
+        target: targetBucket,
+        envName: enviroment // Ajouté pour correspondre à l'utilisation dans validateInput
+    ]
 
-     sh """
-      set -e 
-      aws s3 cp s3://${sourceBucket}  s3://${targetBucket} --recursive
-     """
-     
-     
+    validateInput(config)
+    defineEnvironement(enviroment)
+    checkBucketExistence(config.source)
+    checkBucketExistence(config.target)
+    s3copy(config.source, config.target)
+}
+
+def s3copy(String sourceBucket, String targetBucket) {
+    echo "***** This step will do a copy of the content from the **** : ${sourceBucket}"
+    
+    def confirmation = input(
+        id: 'confirmCopy',
+        message: "ATTENTION, Are you sure you want to copy the content of s3://${sourceBucket} to s3://${targetBucket}?",
+        parameters: [
+            string(name: 'CONFIRM', description: 'Enter YES to continue', defaultValue: '')
+        ]
+    )
+
+    if (confirmation != "YES") {
+        error "Action is aborted, the confirmation 'YES' was not recieved."
+    }
+
+    sh """
+        set -e
+        echo "Content of Source bucket : (${sourceBucket}) :"
+        aws s3 ls s3://${sourceBucket} --recursive --human-readable --summarize
+        
+        echo "Content of target bucket (${targetBucket}) :"
+        aws s3 ls s3://${targetBucket} --recursive --human-readable --summarize
+        
+        echo "launch the copy process"
+        aws s3 cp s3://${sourceBucket} s3://${targetBucket} --recursive
+        
+        echo "Copy process is done, please check the new content of your bucket down below"
+        echo "New Content of target bucket : (${targetBucket}) :"
+        aws s3 ls s3://${targetBucket} --recursive --human-readable --summarize
+    """
+}
+
+def defineEnvironement(enviroment) {
+    if (enviroment == "prod" || enviroment == "learn" || enviroment == "val") {
+        sendEmailApproval(env.BUILD_TAG)
+        timeout(time: 1, unit: 'DAYS') {
+            input message: 'The build need an approval to continue. Do you want to approve this?',
+                  submitter: "${env.APPROVAL_BUILD_USERS}"
+        }
+        echo 'Deployment Approved and Continue!'
+        sendEmailApproval(env.BUILD_TAG, true)
+    } else {
+        echo 'this is dev enviroment'
+    }
+}
+
+def validateInput(Map config) {
+    if (config.envName != "dev") {
+        error "dev enviroment is required, note that val, stg and prod is forbidden for this action"
+    }
+    if (!config.source) {
+        error "this input is empty, Source Bucket is required"
+    }
+    if (!config.target) {
+        error "this input is empty, target Bucket is required"
+    }
+}
+
+def checkBucketExistence(String bucketName) {
+    echo "Verify the existence of : ${bucketName}"
+    
+    def status = sh(
+        script: "aws s3api head-bucket --bucket ${bucketName}",
+        returnStatus: true
+    )
+
+    if (status == 0) {
+        echo "OK : Bucket '${bucketName}' existe."
+    } else {
+        error "ERREUR : Bucket '${bucketName}' doesn't existe or not accessible."
+    }
 }
