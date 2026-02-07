92ac386a168b3df925839b30b88dbc227664a2ef
minalmania04-web <mina.lmania04@gmail.com>
2026-02-08 00:01:30 +0100

    Update copyDB.groovy

--- vars/copyDB.groovy
+++ vars/copyDB.groovy
@@ -60,7 +60,7 @@ def defineEnvironment(Map config) {
     def dbregion = "eu-west-1"
     def db_env = sh(
         script: """
-            TABLE_ARN=\$(aws dynamodb describe-table --table-name ${config.source} --query Table.TableArn --region ${dbregion) --output text }
+            TABLE_ARN=\$(aws dynamodb describe-table --table-name ${config.source} --region ${dbregion} --query Table.TableArn --output text)
             aws dynamodb list-tags-of-resource --resource-arn \$TABLE_ARN --query "Tags[?Key=='env'].Value" --output text 
         """,
         returnStdout: true
